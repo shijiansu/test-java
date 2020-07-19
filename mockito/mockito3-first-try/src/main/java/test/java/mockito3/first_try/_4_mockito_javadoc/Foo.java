f6ab4509d403f13613f74b13b2a06a4b66bc94fa
Shijian Su <sushijian.iss.nus@gmail.com>
2020-07-20 00:07:49 +0800

    Add examples of Mockito JavaDoc

--- mockito/mockito3-first-try/src/main/java/test/java/mockito3/first_try/_4_mockito_javadoc/Foo.java
+++ mockito/mockito3-first-try/src/main/java/test/java/mockito3/first_try/_4_mockito_javadoc/Foo.java
@@ -1,5 +1,8 @@
 package test.java.mockito3.first_try._4_mockito_javadoc;
 
+import java.util.List;
+
+@MyAnnotation
 public class Foo {
   public static String method() {
     System.out.println();
@@ -8,5 +11,34 @@ public class Foo {
 
   public String foo() {
     return "foo";
+  }
+
+  public String someMethod() {
+    return null;
+  }
+
+  public String someMethod(String value) {
+    return value;
+  }
+
+  public String someMethod(List<String> values) {
+    return values.get(0);
+  }
+
+  public Person doSomething(Person person) {
+    person.setName("foo");
+    return person;
+  }
+
+  public void bar() {}
+
+  List<String> bar2() {
+    return null;
+  }
+
+  public void doSomething() {
+  }
+
+  public void doSomething(String s1, String s2, String s3) {
   }
 }
