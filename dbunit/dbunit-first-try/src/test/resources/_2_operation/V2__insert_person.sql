b226f9991febd5a4683eb4fee6667ae8fc6de6c3
Shijian Su <sushijian.iss.nus@gmail.com>
2020-07-25 11:21:24 +0800

    Add example of DBUnit with JUnit4

---
+++ dbunit/dbunit-first-try/src/test/resources/_2_operation/V2__insert_person.sql
@@ -0,0 +1,6 @@
+INSERT INTO person(id, name, age, location)
+VALUES (1, 'tester1', 10, 'Beijing');
+INSERT INTO person(id, name, age, location)
+VALUES (2, 'tester2', 20, 'Shanghai');
+INSERT INTO person(id, name, age, location)
+VALUES (3, 'tester3', 30, 'Singapore');
